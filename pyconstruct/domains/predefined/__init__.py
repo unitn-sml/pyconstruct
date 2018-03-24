2c8f10aa3e3a3be406a0d1301eafb87326c492c8
Paolo Dragone <dragone.paolo@gmail.com>
2018-03-24 16:00:36 +0100

    domains/predefined: small fix in module docstring

--- pyconstruct/domains/predefined/__init__.py
+++ pyconstruct/domains/predefined/__init__.py
@@ -2,7 +2,7 @@
 Pyconstruct provides a number of predefined domains. Predefined domains can
 simply be loaded by::
 
-    domain = Domain('name')
+    domain = Domain(name)
 
 For instance::
 
@@ -11,6 +11,6 @@ For instance::
 Loads the OCR domain, compatible with the OCR dataset.
 
 Currently available:
-  - `ocr`: Optical Character Recognition
-  - `conll00`: CoNLL text chunking
+  - `ocr` : Optical Character Recognition
+  - `conll00` : CoNLL text chunking
 """
