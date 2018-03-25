addf2de01d8f8b80f3993e81ec6eec86181374d2
Paolo Dragone <dragone.paolo@gmail.com>
2018-03-25 16:05:24 +0200

    learners/subgradient: add docstrings to all classes

--- pyconstruct/learners/subgradient.py
+++ pyconstruct/learners/subgradient.py
@@ -31,10 +31,10 @@ def _project_simplex(v, radius=1.0):
     https://gist.github.com/daien/1272551
 
     Algorithm from:
-    [1] Efficient Projections onto the .1-Ball for Learning in High Dimensions
-        John Duchi, Shai Shalev-Shwartz, Yoram Singer, and Tushar Chandra.
-        International Conference on Machine Learning (ICML 2008)
-        http://www.cs.berkeley.edu/~jduchi/projects/DuchiSiShCh08.pdf
+    Efficient Projections onto the .1-Ball for Learning in High Dimensions
+    John Duchi, Shai Shalev-Shwartz, Yoram Singer, and Tushar Chandra.
+    International Conference on Machine Learning (ICML 2008)
+    http://www.cs.berkeley.edu/~jduchi/projects/DuchiSiShCh08.pdf
     """
     n, = v.shape
     if v.sum() == radius and np.alltrue(v >= 0):
@@ -54,10 +54,10 @@ def _project_l1(v, radius=1.0):
     https://gist.github.com/daien/1272551
 
     Algorithm from:
-    [1] Efficient Projections onto the .1-Ball for Learning in High Dimensions
-        John Duchi, Shai Shalev-Shwartz, Yoram Singer, and Tushar Chandra.
-        International Conference on Machine Learning (ICML 2008)
-        http://www.cs.berkeley.edu/~jduchi/projects/DuchiSiShCh08.pdf
+    Efficient Projections onto the .1-Ball for Learning in High Dimensions
+    John Duchi, Shai Shalev-Shwartz, Yoram Singer, and Tushar Chandra.
+    International Conference on Machine Learning (ICML 2008)
+    http://www.cs.berkeley.edu/~jduchi/projects/DuchiSiShCh08.pdf
     """
     n, = v.shape
     u = np.abs(v)
@@ -68,6 +68,29 @@ def _project_l1(v, radius=1.0):
 
 
 class BaseSSG(BaseLearner, ABC):
+    """Base implementation of a learner with the subgradient algorithm.
+
+    This class implements the backbone of a learner that uses the Stochastic
+    Subgradient descent method [1]_ for updating the weights of a linear model.
+    The `partial_fit` method takes care of making inference and calling a
+    gradient step for each example in the batch. Implementations may use
+    different methods for making a gradient step.
+
+    Parameters
+    ----------
+    domain : BaseDomain
+        The domain of the data.
+    inference : str in ['map', 'loss_augmented_map']
+        Which type of inference to perform when learning.
+    structured_loss : function (y, y) -> float
+        The structured loss to compute on the objects.
+
+    References
+    ----------
+    .. [1] Ratliff, Nathan D., J. Andrew Bagnell, and Martin A. Zinkevich.
+        "(Online) Subgradient Methods for Structured Prediction." Artificial
+        Intelligence and Statistics. 2007.
+    """
 
     def __init__(
         self, domain=None, inference='loss_augmented_map', structured_loss=None
@@ -136,6 +159,64 @@ class BaseSSG(BaseLearner, ABC):
 
 
 class SSG(BaseSSG):
+    """Learner implementing the standard subgradient algorithm.
+
+    This learner performs the standard Stochastic Subgradient descent from [1]_.
+    It also includes the options for:
+     - Training with the Pegasos update scheme [2]_; simply set `alpha` greater
+       than zero to regularize the model.
+     - Project onto an L2 or an L1 ball of a given radius, the latter using the
+       projection algorithm from [3]_.
+     - Boost the model with the method from [4]_; simply use a different
+       training loss that `hinge`.
+     - Adaptive step size techniques and such (coming soon).
+
+    Parameters
+    ----------
+    domain : BaseDomain
+        The domain of the data.
+    inference : str in ['map', 'loss_augmented_map']
+        Which type of inference to perform when learning.
+    alpha : float
+        The regularization coefficient.
+    train_loss : str in ['hinge', 'logistic', 'exponential']
+        The training loss. The derivative of this loss is used to rescale the
+        margin of the examples when making an update.
+    projection : None or str in ['l1', 'l2']
+        If None, no projection is applied, otherwise, if 'l1' or 'l2' are given,
+        the weights are projected back onto an L1 or an L2 ball respectively.
+    radius : float
+        The radius of the ball onto which project the weights when using
+        projection.
+    eta0 : float
+        The initial value of the learning rate.
+    power_t : float
+        The power of the iteration index when using an `invscaling`
+        learning_rate.
+    learning_rate : str in ['constant', 'optimal', 'invscaling']
+        The learning rate strategy. The `constant` learning multiplies the
+        updates for `eta0`; the `invscaling` divides the updates by the
+        iteration number raised to the `power_t`; the `optimal` strategy finds
+        the best rate depending on `alpha` and `train_loss` (similar to
+        Scikit-learn's SGDRegressor `optimal` learning rate).
+    structured_loss : function (y, y) -> float
+        The structured loss to compute on the objects.
+
+    References
+    ----------
+    .. [1] Ratliff, Nathan D., J. Andrew Bagnell, and Martin A. Zinkevich.
+        "(Online) Subgradient Methods for Structured Prediction." Artificial
+        Intelligence and Statistics. 2007.
+    .. [2] Shalev-Shwartz, Shai, et al. "Pegasos: Primal estimated sub-gradient
+        solver for svm." Mathematical programming 127.1 (2011): 3-30.
+    .. [3] Efficient Projections onto the .1-Ball for Learning in High
+        Dimensions John Duchi, Shai Shalev-Shwartz, Yoram Singer, and Tushar
+        Chandra. International Conference on Machine Learning (ICML 2008)
+        http://www.cs.berkeley.edu/~jduchi/projects/DuchiSiShCh08.pdf
+    .. [4] Parker, Charles, Alan Fern, and Prasad Tadepalli. "Gradient boosting
+        for sequence alignment." Proceedings of the 21st national conference on
+        Artificial intelligence-Volume 1. AAAI Press, 2006.
+    """
 
     def __init__(
         self, *, domain=None, inference='loss_augmented_map', alpha=0.0001,
@@ -217,7 +298,44 @@ class SSG(BaseSSG):
 
 
 class EG(BaseSSG):
-    """Exponentiated gradient algorithm."""
+    """Learner implementing the Exponentiate Gradient algorithm.
+
+    This learner uses multiplicative weight updates as in [1]_.
+
+    Parameters
+    ----------
+    domain : BaseDomain
+        The domain of the data.
+    inference : str in ['map', 'loss_augmented_map']
+        Which type of inference to perform when learning.
+    alpha : float
+        The regularization coefficient.
+    train_loss : str in ['hinge', 'logistic', 'exponential']
+        The training loss. The derivative of this loss is used to rescale the
+        margin of the examples when making an update.
+    radius : float
+        This property is only used here to decide when to clip the exponential
+        training loss.
+    eta0 : float
+        The initial value of the learning rate.
+    power_t : float
+        The power of the iteration index when using an `invscaling`
+        learning_rate.
+    learning_rate : str in ['constant', 'optimal', 'invscaling']
+        The learning rate strategy. The `constant` learning multiplies the
+        updates for `eta0`; the `invscaling` divides the updates by the
+        iteration number raised to the `power_t`; the `optimal` strategy finds
+        the best rate depending on `alpha` and `train_loss` (similar to
+        Scikit-learn's SGDRegressor `optimal` learning rate).
+    structured_loss : function (y, y) -> float
+        The structured loss to compute on the objects.
+
+    References
+    ----------
+    .. [1] Collins, Michael, et al. "Exponentiated gradient algorithms for
+        conditional random fields and max-margin markov networks." Journal of
+        Machine Learning Research 9.Aug (2008): 1775-1822.
+    """
 
     def __init__(
         self, *, domain=None, inference='loss_augmented_map', alpha=0.0001,
