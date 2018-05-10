92fae044a2a7d3d576c3df86f677120e29536dae
Paolo Dragone <dragone.paolo@gmail.com>
2018-05-10 10:53:29 +0200

    learners/perceptron: fix according to changes to SSG

--- pyconstruct/learners/perceptron.py
+++ pyconstruct/learners/perceptron.py
@@ -24,14 +24,16 @@ class StructuredPerceptron(SSG):
     """
     def __init__(self, domain=None):
         super().__init__(
-            domain=domain, projection=None, alpha=0.0, learning_rate='constant',
+            domain, projection=None, alpha=0.0, learning_rate='constant',
             inference='map'
         )
 
-    def _step(self, w, x, y_true, y_pred):
+    def _step(self, x, y_true, y_pred, phi_y_true, phi_y_pred, w, eta):
         score_true = self.model.decision_function(*asarrays(x, y_true))
         score_pred = self.model.decision_function(*asarrays(x, y_pred))
         if w is None or score_true < score_pred:
-            return super()._step(w, x, y_true, y_pred)
-        return w
+            return super()._step(
+                x, y_true, y_pred, phi_y_true, phi_y_pred, w, eta
+            )
+        return 0.0
 
