e956f22ffaef9f07efa0a7f7f4111c2de8975cd5
Paolo Dragone <dragone.paolo@gmail.com>
2018-05-14 09:13:32 +0200

    utils/cache: major improvements

--- pyconstruct/utils/cache.py
+++ pyconstruct/utils/cache.py
@@ -1,6 +1,5 @@
 
 from sklearn.externals import joblib
-from collections.abc import Mapping
 
 try:
     from cachetools.keys import _kwmark
@@ -11,27 +10,32 @@ except ImportError:
 __all__ = ['hashkey', 'typedkey']
 
 
+def _flatten(item):
+    flat = []
+    if isinstance(item, (list, tuple)):
+        flat += [_flatten(e) for e in item]
+    elif isinstance(item, dict):
+        flat += [_flatten(e) for e in sum(sorted(item.items()), _kwmark)]
+    else:
+        return (item,)
+    return tuple(flat)
+
+
 class HashableArgs:
+
 
     def __init__(self, *args, **kwargs):
         self.args = args
         self.kwargs = kwargs
-        _hashkey = self.args + sum(sorted(self.kwargs.items()), _kwmark)
-        self._hashkey = []
-        for arg in _hashkey:
-            if isinstance(arg, dict):
-                self._hashkey += list(sum(sorted(arg.items()), _kwmark))
-            else:
-                self._hashkey.append(arg)
-        self._hashkey = tuple(self._hashkey)
+        self._hashkey = _flatten(self.args) + _flatten(self.kwargs)
         self._hashvalue = None
 
     def __eq__(self, other):
        if len(self._hashkey) != len(other._hashkey):
            return False
 
-       for arg1, arg2 in zip(self._hashkey, other._hashkey):
-           if arg1 != arg2:
+       for o1, o2 in zip(self._hashkey, other._hashkey):
+           if o1 != o2:
                return False
        return True
 
@@ -40,7 +44,7 @@ class HashableArgs:
             o1, o2 = None, None
             if i < len(self._hashkey):
                 o1 = self._hashkey[i]
-            if i < len(self._hashkey):
+            if i < len(other._hashkey):
                 o2 = other._hashkey[i]
             if o1 is None:
                 return True
@@ -48,28 +52,13 @@ class HashableArgs:
                 return False
             if type(o1) != type(o2):
                 return type(o1).__name__ < type(o2).__name__
-            if isinstance(o1, Mapping):
-                o1_items = sorted(o1.items())
-                o2_items = sorted(o2.items())
-                for o1_item, o2_item in zip(o1_items, o2_items):
-                    o1_key, o1_value = o1_item
-                    o2_key, o2_value = o2_item
-                    if o1_key < o2_key:
-                        return True
-                    if o1_key > o2_key:
-                        return False
-                    if o1_value < o2_value:
-                        return True
-                    if o1_value > o2_value:
-                        return False
-            else:
-                try:
-                    if o1 > o2:
-                        return False
-                    if o1 < o2:
-                        return True
-                except TypeError:
+            try:
+                if o1 > o2:
                     return False
+                if o1 < o2:
+                    return True
+            except TypeError:
+                return False
         return False
 
     def __gt__(self, other):
@@ -77,7 +66,7 @@ class HashableArgs:
             o1, o2 = None, None
             if i < len(self._hashkey):
                 o1 = self._hashkey[i]
-            if i < len(self._hashkey):
+            if i < len(other._hashkey):
                 o2 = other._hashkey[i]
             if o2 is None:
                 return True
@@ -85,28 +74,13 @@ class HashableArgs:
                 return False
             if type(o1) != type(o2):
                 return type(o1).__name__ > type(o2).__name__
-            if isinstance(o1, Mapping):
-                o1_items = sorted(o1.items())
-                o2_items = sorted(o2.items())
-                for o1_item, o2_item in zip(o1_items, o2_items):
-                    o1_key, o1_value = o1_item
-                    o2_key, o2_value = o2_item
-                    if o1_key > o2_key:
-                        return True
-                    if o1_key < o2_key:
-                        return False
-                    if o1_value > o2_value:
-                        return True
-                    if o1_value < o2_value:
-                        return False
-            else:
-                try:
-                    if o1 < o2:
-                        return False
-                    if o1 > o2:
-                        return True
-                except TypeError:
+            try:
+                if o1 < o2:
                     return False
+                if o1 > o2:
+                    return True
+            except TypeError:
+                return False
         return False
 
     def __hash__(self):
