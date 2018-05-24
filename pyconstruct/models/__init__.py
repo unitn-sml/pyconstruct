77dd6b8ba5d8ba5f5b44412644ed04346f1fd8ed
Paolo Dragone <dragone.paolo@gmail.com>
2018-05-24 15:20:25 +0200

    models: fix docstring

--- pyconstruct/models/__init__.py
+++ pyconstruct/models/__init__.py
@@ -1,26 +1,29 @@
 """\
-Pyconstructi abstracts predictive models using a `Model` class. A `Model` is an
-object that holds some kind of parameters that can be used to make inference.
+Pyconstructi abstracts predictive models using the `BaseModel` class. Classes
+that inherit from `BaseModel` are objects holding the parameters of some
+mathematical model and that can "communicate" with domains in order to make
+prediction by solving inference problems.
 
 When making inference, the model's parameters are passed to the Domain, which
 needs to be able to interpret the semantics of the parameters. For instance, the
 standard model provided by Pyconstruct is a `LinearModel`, whose parameters
-consist in just a weight vector. Domains using the `solve` macro from the
-`pyconstruct.pmzn` file (see the `MiniZincDomain` class documentation for
-details) are readily capable of interpreting this weight vector and use it to
-compute the dot product with the feature vector.
+consist in just a weight vector (and some optional additional features). Domains
+using the `linear_model` macro from the `linear.pmzn` file (see the `domains`
+module documentation for details) are readily capable of interpreting this
+weight vector and use it to compute the dot product with the feature vector.
 
 While linear models cover many of the cases in structured-output prediction, one
-can easily think of cases in which an ad-hoc `Model` may be beneficial, e.g.
+can easily think of cases in which an ad-hoc model may be beneficial, e.g.
 when the model has some hyper-paramenters or when performing some sort of
 feature learning. That said, in most cases you should not need to manipulate
-`Models` directly.
+models directly.
 
-The `Model` is the middle-men between a `Learner` and a `Domain`. The learner
-fits a `Model`, which is in turn then passed to the `Domain` to perform
-inference. Usually a `Learner` is only capable of learning one type of models,
-while a `Domain` may usually be used with different `Models` (if properly
-configured).
+The model is the middle-men between a learner and a domain. The learner fits
+the parameter of a model, without knowing how inference is carried out. The
+domain, on the other hand, receives the model's parameters, without knowing how
+these are learned. Usually a learner is only capable of learning one type of
+models (and derived ones), while a domain may conditionally be used with
+different models (if properly configured).
 """
 
 from . import base
